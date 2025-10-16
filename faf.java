d4f4d7ecad69e10cfc534c8a9e9ef8b25a025be1
Filipe <filipe.lopes@aluno.ifsertao-pe.edu.br>
2025-10-16 12:06:52 -0300

    adadda

---
+++ faf.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class faf{
+
+    public static void main(String[] args) {
+        Scanner tecla = new Scanner(System.in);
+
+
+        System.out.println("Olá, mundo!");
+        int num = tecla.nextInt();
+    }
+
+}
