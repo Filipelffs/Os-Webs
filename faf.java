7e4437ec0a910c1558e4c03aa49d2dbcfd5172f9
Filipe <filipe.lopes@aluno.ifsertao-pe.edu.br>
2025-10-19 22:08:17 -0300

    Flamengooo

--- faf.java
+++ faf.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
-public class faf{
+public class faf {
 
     public static void main(String[] args) {
         Scanner tecla = new Scanner(System.in);
 
-
-        System.out.println("Olá, mundo!");
-        int num = tecla.nextInt();
+        System.out.println("Digite um numero");
+        int num1 = tecla.nextInt();
+        System.out.println("Digite o segundo numero");
+        int num2 = tecla.nextInt();
+    
+        int total = num1 += num2;
+        
+        System.out.println("O total da soma foi?" + total);
     }
 
 }
